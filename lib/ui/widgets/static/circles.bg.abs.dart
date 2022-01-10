2f1b6a0832a4ed4ddb9338f4489d0c5349c41e5d
delgerskhn <delgersaikhan2001@gmail.com>
2022-01-10 16:16:46 +0800

    background circles fixed layout

---
+++ lib/ui/widgets/static/circles.bg.abs.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yobit/constants/infrastructure/paths.dart';
+
+class CirclesBgAbs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        child: Container(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(child: Container(), flex: 1),
+                  Expanded(
+                      child: Container(
+                        child: Image.network(
+                          Paths.bgCircleLg,
+                          fit: BoxFit.cover,
+                        ),
+                        margin: EdgeInsets.only(bottom: 100),
+                      ),
+                      flex: 1),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Image.network(
+                      Paths.bgCircleLeftLg,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                          child: Image.network(
+                            Paths.bgCircleMd,
+                            fit: BoxFit.none,
+                          ),
+                          alignment: Alignment.topCenter)),
+                ],
+              ),
+            ],
+          ),
+        ),
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0);
+  }
+}
--- lib/ui/widgets/static/earth.background.dart
+++ lib/ui/widgets/static/earth.background.dart
@@ -4,6 +4,7 @@ import 'package:yobit/ui/widgets/static/star.bg.abs.dart';
 import 'circle.bg.left.lg.abs.dart';
 import 'circle.bg.lg.abs.dart';
 import 'circle.sm.abs.dart';
+import 'circles.bg.abs.dart';
 import 'earth.abs.dart';
 
 typedef WrapperFn = Widget Function();
@@ -18,9 +19,10 @@ class EarthBackground extends StatelessWidget {
         body: Stack(children: <Widget>[
           StarBgAbs(),
           EarthAbs(),
-          CircleBgLgAbs(left: 140, bottom: 570, right: 0, top: 0),
-          CircleBgLeftLgAbs(left: 0, bottom: 170, right: 185, top: 300),
-          CircleBgSmAbs(left: 287, bottom: 485 - 40, right: 60, top: 300 + 40),
+          CirclesBgAbs(),
+          // CircleBgLgAbs(left: 140, bottom: 570, right: 0, top: 0),
+          // CircleBgLeftLgAbs(left: 0, bottom: 170, right: 185, top: 300),
+          // CircleBgSmAbs(left: 287, bottom: 485 - 40, right: 60, top: 300 + 40),
           Positioned(child: this.child(), top: 0, bottom: 0, right: 0, left: 0)
         ]),
         backgroundColor: Colors.black);
