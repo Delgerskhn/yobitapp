9cd918f142b57d4cac3c03e8cb9878b2dda7a505
amarjargalamraa61 <amarjargalamraa61@gmail.com>
2022-02-13 17:51:36 +0800

    za

--- lib/ui/screens/challenge.dart
+++ lib/ui/screens/challenge.dart
@@ -1,5 +1,10 @@
 //TODO: create the challenge screen here
 import 'package:flutter/material.dart';
+import 'package:yobit/ui/widgets/ChallengeWidgets/challengeRuby.dart';
+import 'package:yobit/ui/widgets/ChallengeWidgets/challengeSponser.dart';
+import 'package:yobit/ui/widgets/ChallengeWidgets/challengeStars.dart';
+import 'package:yobit/ui/widgets/static/star.background.dart';
+import 'package:yobit/ui/widgets/static/star.bg.abs.dart';
 
 class ChallengeScreen extends StatelessWidget {
   const ChallengeScreen({
@@ -9,281 +14,101 @@ class ChallengeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          color: Colors.white,
-          onPressed: () {},
-        ),
-        actions: [
-          Padding(
-              padding: const EdgeInsets.only(right: 30, top: 12),
-              child: Image.asset(
-                "assets/profile.png",
-                // scale: 2.3,
-                width: 35,
-                height: 25,
-              )
-              // child: const Icon(Icons.ac_unit, color: Colors.green),
-              )
-        ],
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/Stars.png"),
-            fit: BoxFit.cover,
+        appBar: AppBar(
+          leading: BackButton(
+            color: Colors.white,
+            onPressed: () {},
           ),
-          gradient: LinearGradient(
-              colors: [Color(0xff080712), Color(0xff201F3A)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter),
+          actions: [
+            Padding(
+                padding: const EdgeInsets.only(right: 30, top: 12),
+                child: Image.asset(
+                  "assets/images/profile.png",
+                  // scale: 2.3,
+                  width: 35,
+                  height: 25,
+                )
+                // child: const Icon(Icons.ac_unit, color: Colors.green),
+                )
+          ],
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
+        body: StarBackground(
+          child: () => Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  Positioned(
-                    top: 100,
-                    left: 20,
-                    right: 20,
-                    child: SizedBox(
-                      width: double.infinity,
-                      //  new CustomPaint(painter: new Rectangle())
-                      child: Container(
-                        width: double.infinity,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF0099FF),
-                        ),
-                      ),
-                    ),
-                  ),
+                  ChallengeStars(),
+                  ChallengeRuby(),
+                  ChallengeSponser(),
                   Container(
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          "assets/Garig4.png",
-                          width: 200,
-                          height: 200,
-                        ),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          padding: EdgeInsets.only(left: 40),
-                          child: Text(
-                            "Кпс-ийн гариг",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
+                    margin: const EdgeInsets.only(top: 30),
+                    child: ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width * .2,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.arrow_back,
+                                  color: Colors.deepOrangeAccent, size: 30),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.deepOrange),
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.red),
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Stack(
-                children: [
-                  Container(
-                    padding:
-                        const EdgeInsets.only(left: 20, top: 15, bottom: 15),
-                    margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              "assets/ruby.png",
-                              width: 80,
+                          Container(
+                            width: MediaQuery.of(context).size.width * .6,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Оролцох",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  Icon(Icons.arrow_forward_ios,
+                                      color: Colors.amber, size: 30)
+                                ],
+                              ),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.deepOrange),
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.red),
+                                  ),
+                                ),
+                              ),
                             ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Тайлбар",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  "14 оролцогч - 7 даалгавар",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                        color: const Color(0xFF0099FF),
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  Positioned(
-                    right: 50,
-                    top: 20,
-                    child: Icon(Icons.star, color: Colors.amber, size: 30),
                   )
                 ],
               ),
-              Container(
-                padding: const EdgeInsets.only(left: 20, top: 35, bottom: 35),
-                margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset(
-                          "assets/kfc.png",
-                          width: 80,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Шагнал",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "50.000 төгрөгт багтах бүтээгдэхүүний урамшуулал",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    color: const Color(0xFF0099FF),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 30),
-                child: ListTile(
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * .2,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.arrow_back,
-                              color: Colors.deepOrangeAccent, size: 30),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.deepOrange),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.red),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * .6,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.arrow_forward_ios,
-                              color: Colors.amber, size: 30),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.deepOrange),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.red),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-              // Stack(
-              //   children: [
-              //     Positioned(
-              //       top: 15,
-              //       left: 20,
-              //       right: 20,
-              //       child: SizedBox(
-              //         width: double.infinity,
-              //         // new CustomPaint(painter: new Rectangle())
-              //         child: Container(
-              //           width: MediaQuery.of(context).size.width * .8,
-              //           height: MediaQuery.of(context).size.width * .2,
-              //           decoration: BoxDecoration(
-              //               color: const Color(0xFF0099FF),
-              //               borderRadius: BorderRadius.circular(10)),
-              //         ),
-              //       ),
-              //     ),
-              //     Padding(
-              //       padding: const EdgeInsets.only(top: 50, left: 20, right: 0),
-              //       child: Column(
-              //         children: [
-              //           Row(
-              //             children: [
-              //               Image.asset(
-              //                 "assets/kfc.png",
-              //                 width: MediaQuery.of(context).size.width * .3,
-              //               ),
-              //               Column(children: [
-              //                 Text(
-              //                   "Шагнал",
-              //                   style: TextStyle(
-              //                     color: Colors.white,
-              //                     fontSize: 20,
-              //                     fontWeight: FontWeight.bold,
-              //                   ),
-              //                 ),
-              //                 Text(
-              //                   "50.000 төгрөгт багтах бүтээгдэхүүний урамшуулал",
-              //                   style: TextStyle(
-              //                     color: Colors.white,
-              //                     fontSize: 10,
-              //                     // fontWeight: FontWeight.bold,
-              //                   ),
-              //                 ),
-              //               ])
-              //             ],
-              //           ),
-              //         ],
-              //       ),
-              //     ),
-              //   ],
-              // ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
