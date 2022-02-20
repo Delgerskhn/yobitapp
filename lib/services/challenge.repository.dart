5722a7422471807890ff5f3d300072e13d2725f1
delgerskhn <delgersaikhan2001@gmail.com>
2022-02-20 14:45:11 +0800

    challenge cached network image, not need to get image download url instead provided from backend

--- lib/services/challenge.repository.dart
+++ lib/services/challenge.repository.dart
@@ -4,6 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:yobit/logic/models/challenge.dart';
 import 'package:yobit/utils/httpClient.dart';
 
+Future<List<Challenge>> getFeaturedChallenges() async {
+  final response = await HttpClient.get('challenge/featured');
+  List<dynamic> res = jsonDecode(response.body);
+  return res.map((e) => Challenge.fromJson(e)).toList();
+}
+
 Future<List<Challenge>> getTopChallenges() async {
   final response = await HttpClient.get('challenge');
 
--- lib/ui/widgets/home/slider.dart
+++ lib/ui/widgets/home/slider.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:yobit/constants/infrastructure/paths.dart';
@@ -19,7 +20,7 @@ class _Slider extends State<Slider> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: getTopChallenges(),
+        future: getFeaturedChallenges(),
         builder: (ctx, snapshot) {
           if (snapshot.connectionState == ConnectionState.done &&
               snapshot.hasError) {
@@ -31,6 +32,7 @@ class _Slider extends State<Slider> {
             );
           } else if (snapshot.hasData) {
             final challenges = snapshot.data as List<Challenge>;
+            print(challenges);
             return Column(children: [
               Container(
                 child: CarouselSlider(
@@ -46,54 +48,50 @@ class _Slider extends State<Slider> {
                       }),
                   carouselController: controller,
                   items: challenges.map((challenge) {
-                    return FutureBuilder(
-                      future: instance.ref(challenge.imgUrl).getDownloadURL(),
-                      builder: (context, snapshot) {
-                        print(snapshot.data);
-                        print(snapshot.error);
-                        if (snapshot.hasData) {
-                          return Container(
-                            child: Container(
-                              margin: EdgeInsets.all(5),
-                              child: InkWell(
-                                  child: ClipRRect(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(25.0)),
-                                      child: Stack(
-                                        children: [
-                                          Image.network(
-                                            snapshot.data as String,
-                                            fit: BoxFit.cover,
-                                            width: 1000,
-                                          ),
-                                          Positioned(
-                                            bottom: 0,
-                                            left: 0,
-                                            top: 0,
-                                            right: 0,
-                                            child: Container(
-                                                decoration: BoxDecoration(
-                                                    gradient: LinearGradient(
-                                                        colors: [
-                                                      Color.fromARGB(
-                                                          200, 0, 0, 0),
-                                                      Color.fromARGB(0, 0, 0, 0)
-                                                    ],
-                                                        begin: Alignment
-                                                            .bottomCenter,
-                                                        end: Alignment
-                                                            .topCenter)),
-                                                padding: EdgeInsets.symmetric(
-                                                    vertical: 10,
-                                                    horizontal: 20)),
-                                          )
-                                        ],
-                                      ))),
-                            ),
-                          );
-                        }
-                        return Center(
-                          child: CircularProgressIndicator(),
+                    return Builder(
+                      builder: (context) {
+                        return Container(
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            child: InkWell(
+                                child: ClipRRect(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(25.0)),
+                                    child: Stack(
+                                      children: [
+                                        CachedNetworkImage(
+                                          imageUrl: challenge.imgUrl,
+                                          fit: BoxFit.cover,
+                                          errorWidget: (context, url, error) =>
+                                              Text('Error!'),
+                                          placeholder: (context, url) =>
+                                              CircularProgressIndicator(),
+                                          width: 1000,
+                                        ),
+                                        Positioned(
+                                          bottom: 0,
+                                          left: 0,
+                                          top: 0,
+                                          right: 0,
+                                          child: Container(
+                                              decoration: BoxDecoration(
+                                                  gradient: LinearGradient(
+                                                      colors: [
+                                                    Color.fromARGB(
+                                                        200, 0, 0, 0),
+                                                    Color.fromARGB(0, 0, 0, 0)
+                                                  ],
+                                                      begin: Alignment
+                                                          .bottomCenter,
+                                                      end:
+                                                          Alignment.topCenter)),
+                                              padding: EdgeInsets.symmetric(
+                                                  vertical: 10,
+                                                  horizontal: 20)),
+                                        )
+                                      ],
+                                    ))),
+                          ),
                         );
                       },
                     );
